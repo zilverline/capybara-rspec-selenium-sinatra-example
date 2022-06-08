b19cef442bf708a7c9c4cd23353de3d8ae008b45
Lars Vonk <lars.vonk@gmail.com>
2022-06-08 19:58:36 +0200

    Make screenshot :camera: when specs fail

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,18 @@ RSpec.configure do |config|
     require_relative 'initialize_capybara'
   end
 
+  config.after :each, :features do |example|
+    if example.exception
+      filename = File.basename(example.metadata[:file_path])
+      line_number = example.metadata[:line_number]
+      timestamp = Time.now.strftime('%Y-%m-%d-%H-%M-%S')
+
+      screenshot_path = "/tmp/capybara/#{filename}-#{line_number}-#{timestamp}.png"
+
+      Capybara.page.save_screenshot(screenshot_path)
+      puts "\n"
+      puts "Screenshot: #{screenshot_path}"
+    end
+  end
+
 end
